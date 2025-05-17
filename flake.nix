4a95660e9fa372b11f2601e189740c9c054f4f18
KZdkm <tinghan.zhu@hotmail.com>
2025-05-17 11:31:11 +0800

    fix dependencies

--- flake.nix
+++ flake.nix
@@ -17,7 +17,9 @@
 
       # additional libraries and executables to add to gjs' runtime
       extraPackages = [
+        ags.packages.${system}.apps
         ags.packages.${system}.battery
+        ags.packages.${system}.bluetooth
         ags.packages.${system}.hyprland
         ags.packages.${system}.tray
         ags.packages.${system}.mpris
